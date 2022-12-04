cb236be0220a1f970a22cc78c64c8457a6efc304
JeanCharlie <santanajeankarlo@gmail.com>
2022-12-04 17:14:54 -0500

    its all good man

--- src/main/java/cu/edu/cujae/pweb/bean/ManageCountryBean.java
+++ src/main/java/cu/edu/cujae/pweb/bean/ManageCountryBean.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
-;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
